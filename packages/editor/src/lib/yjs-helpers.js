6e328394c6a4c82dac2589299bcad47d96fc255d
Esteban Primost <esteban.primost@gmail.com>
2019-11-20 17:45:37 -0300

    Export modules

--- packages/editor/src/lib/yjs-helpers.js
+++ packages/editor/src/lib/yjs-helpers.js
@@ -5,3 +5,5 @@
 export const CONTENT_KEY = 'xmlfragment_content_v1';
 
 export const getXmlFragmentContent = doc => doc.getXmlFragment(CONTENT_KEY);
+
+export const getContentAsMarkdown = () => '## HOLA';
