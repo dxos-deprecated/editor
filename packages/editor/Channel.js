d700bcfff58005937a647b9c6d59654bf2780c0b
Rich Burdon <rich.burdon@gmail.com>
2019-12-22 11:23:36 -0500

    Remove top-level classes (replaced with index.js)

--- packages/editor/Channel.js
+++
@@ -1,6 +0,0 @@
-//
-// Copyright 2019 Wireline, Inc.
-//
-
-// TODO(burdon): Remove.
-module.exports = require('./dist/es/lib/Channel');
--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -2,9 +2,7 @@
 // Copyright 2019 Wireline, Inc.
 //
 
-// TODO(burdon): Why lib?
 import Channel from './lib/Channel';
-
 import Editor from './components/Editor';
 
 // TODO(burdon): Why is this exported?
